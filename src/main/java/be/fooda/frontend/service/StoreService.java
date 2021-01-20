f15b02ff5b7407d2a8ef38a683408f9c6a491527
siri2021 <siridiya_gmail.com>
2021-01-21 00:30:48 +0100

    storeService

--- src/main/java/be/fooda/frontend/service/StoreService.java
+++ src/main/java/be/fooda/frontend/service/StoreService.java
@@ -1,25 +1,28 @@
 package be.fooda.frontend.service;
 
 import be.fooda.frontend.models.product.Product;
-import be.fooda.frontend.models.store.store;
+import be.fooda.frontend.models.store.Store;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @Service
-public class storeService {
+public class StoreService {
     private final RestTemplate restTemplate;
 
     @Value("${fooda.store.service.url}")
     private String baseUrl;
 
-    public storeService(RestTemplate restTemplate) {
+    public StoreService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
@@ -64,6 +67,53 @@ public class storeService {
         queryParams.put("id",id);
         return restTemplate.exchange(completeUrl, HttpMethod.GET, HttpEntity.EMPTY, String.class, queryParams);
     }
+    public ResponseEntity storeExists(Store store){
+        final String completeUrl = baseUrl + "store_Exists";
+        return restTemplate.exchange(completeUrl, HttpMethod.GET, new HttpEntity<>(store), String.class);
+    }
+    public ResponseEntity searchByAddress(String postcode,String municipality,int pageNo, int pageSize){
+        final String completeUrl = baseUrl + "search_By-Address?postcode={postcode}&municipality={municipality}&pageNo={pageNo}&pageSize={pageSize}";
+        Map<String, Object> queryParams = new HashMap<>();
+        queryParams.put("postcode",postcode);
+        queryParams.put("municipality",municipality);
+        queryParams.put("pageNo", pageNo);
+        queryParams.put("pageSize", pageSize);
+
+        return restTemplate.exchange(completeUrl, HttpMethod.GET, HttpEntity.EMPTY, Store[].class, queryParams);
+    }
+    public ResponseEntity filterByDeliveryCosts(BigDecimal minPrice,BigDecimal maxPrice,int pageNo, int pageSize) {
+        final String completeUrl = baseUrl + "filter_By_Delivery_Costs?minPrice={minPrice}&maxPrice={maxPrice}&pageNo={pageNo}&pageSize={pageSize}";
+        Map<String, Object> queryParams = new HashMap<>();
+        queryParams.put("maxPrice", maxPrice);
+        queryParams.put("minPrice", minPrice);
+        queryParams.put("pageNo", pageNo);
+        queryParams.put("pageSize", pageSize);
+        return restTemplate.exchange(completeUrl, HttpMethod.GET, HttpEntity.EMPTY, Store[].class, queryParams);
+    }
+
+    public ResponseEntity filterByDeliveryDuration(Double minDuration, Double maxDuration, Integer pageNo,  Integer pageSize){
+        final String completeUrl = baseUrl + " filter_By_Delivery_Duration?minDuration={minDuration}&minDuration={minDuration}&pageNo={pageNo}&pageSize={pageSize";
+        Map<String, Object> queryParams = new HashMap<>();
+        queryParams.put("minDuration", minDuration);
+        queryParams.put("maxDuration", maxDuration);
+        queryParams.put("pageNo", pageNo);
+        queryParams.put("pageSize", pageSize);
+        return restTemplate.exchange(completeUrl, HttpMethod.GET, HttpEntity.EMPTY, Store[].class, queryParams);
+    }
+
+    public ResponseEntity searchByMenuItemsName( String menuItem,  Integer pageNo,  Integer pageSize){
+        final String completeUrl = baseUrl +"search_By_Menu_ItemName?menuItem={menuItem}&pageNo={pageNo}&pageSize={pageSize";
+        Map<String, Object> queryParams = new HashMap<>();
+
+        queryParams.put("menuItem", menuItem);
+        queryParams.put("pageNo", pageNo);
+        queryParams.put("pageSize", pageSize);
+        return restTemplate.exchange(completeUrl, HttpMethod.GET, HttpEntity.EMPTY, Store.class, queryParams);
+    }
+  /*  public ResponseEntity searchByCuisine(String cuisine,Integer pageNo,  Integer pageSize){
+        final String completeUrl = baseUrl + "search_By_Cuisine"
+    }*/
+
 
 
 
