827c15d380a2896b60a5a868f18aafc65e82c66a
ozdemir-ahmet <ozdemir76@yahoo.com>
2021-01-21 00:20:02 +0100

    service: contact and address services updated

---
+++ src/main/java/be/fooda/frontend/service/AddressService.java
@@ -0,0 +1,53 @@
+package be.fooda.frontend.service;
+
+import be.fooda.frontend.models.address.Address;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class AddressService {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${fooda.address.service.url}")
+    private String baseUrl;
+
+    public AddressService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public ResponseEntity addAddress(Address address) {
+        final String completeUrl = baseUrl + "add_address";
+        return restTemplate.exchange(completeUrl, HttpMethod.POST, new HttpEntity<>(address), String.class);
+    }
+
+    public ResponseEntity getAll() {
+        final String completeUrl = baseUrl + "get_All_Addresses";
+        return restTemplate.exchange(completeUrl, HttpMethod.GET, HttpEntity.EMPTY, Address[].class);
+    }
+
+    public ResponseEntity getAddressById(Long id) {
+        final String completeUrl = baseUrl + "get_Address_by_id?id={id}";
+        return restTemplate.exchange(completeUrl, HttpMethod.GET, HttpEntity.EMPTY, Address.class, id);
+    }
+
+    public ResponseEntity getAddressByUserId(Long externalUserId){
+        final String completeUrl = baseUrl + "get_by_external_user_id?externalUserId={externalUserId}";
+        return restTemplate.exchange(completeUrl, HttpMethod.GET, HttpEntity.EMPTY, Address[].class, externalUserId);
+    }
+
+//    @GetMapping("filter_By_Geolocation")
+//    public ResponseEntity filterByGeolocation(@RequestParam String latitude, @RequestParam String longitude) {
+//        return ResponseEntity.status(HttpStatus.FOUND).body(indexRepository.filterByGeolocation(latitude, longitude));
+//    }
+
+    public ResponseEntity deleteAddressById(Long id){
+        final String completeUrl = baseUrl + "delete_address?id={id}";
+        return restTemplate.exchange(completeUrl, HttpMethod.DELETE, HttpEntity.EMPTY, String.class, id);
+    }
+
+}
--- src/main/java/be/fooda/frontend/service/ContactService.java
+++ src/main/java/be/fooda/frontend/service/ContactService.java
@@ -6,12 +6,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
-import javax.validation.constraints.Positive;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -45,13 +41,11 @@ public class ContactService {
         return restTemplate.exchange(completeUrl, HttpMethod.GET, HttpEntity.EMPTY, Contact.class, id);
     }
 
-    @GetMapping("get_Contact_details_By_user_Id")
-    public ResponseEntity getContactByUserId(@RequestParam @Positive Long externalUserId) {
+    public ResponseEntity getContactByUserId(Long externalUserId) {
         final String completeUrl = baseUrl + "get_Contact_details_By_user_Id?externalUserId={externalUserId}";
         return restTemplate.exchange(completeUrl, HttpMethod.GET, HttpEntity.EMPTY, Contact[].class, externalUserId);
     }
 
-    @PostMapping("add_Contact")
     public ResponseEntity addContact(Contact contact) {
         final String completeUrl = baseUrl + "add_Contact";
         return restTemplate.exchange(completeUrl, HttpMethod.POST, new HttpEntity<>(contact), String.class);
