8dcd79963e710f0b07eb7df3cfec1b5ae9feb992
nozha-kannouf <nozha.kannouf@gmail.com>
2021-01-21 11:28:22 +0100

    Customer models added

---
+++ src/main/java/be/fooda/frontend/models/customer/CustomerStore.java
@@ -0,0 +1,37 @@
+package be.fooda.frontend.models.customer;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class CustomerStore {
+
+    @JsonIgnore
+    private Long id;
+
+    private Long externalStoreId;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getExternalStoreId() {
+        return externalStoreId;
+    }
+
+    public void setExternalStoreId(Long externalStoreId) {
+        this.externalStoreId = externalStoreId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
