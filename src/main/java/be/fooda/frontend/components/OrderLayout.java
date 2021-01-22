388d1f40db894a146c64d741ade5f4b53e5cd54f
yilmazchef <yilmaz@mail.be>
2021-01-22 12:22:19 +0100

    basket service addToBasket is added.

--- src/main/java/be/fooda/frontend/components/OrderLayout.java
+++ src/main/java/be/fooda/frontend/components/OrderLayout.java
@@ -1,48 +1,45 @@
 package be.fooda.frontend.components;
 
-import be.fooda.frontend.models.store.Store;
-import be.fooda.frontend.models.store.StoreImageType;
+import be.fooda.frontend.models.order.Order;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.theme.material.Material;
 
+import java.awt.*;
+
 public class OrderLayout extends VerticalLayout {
 
-    public OrderLayout(Store data) {
+    public OrderLayout(Order data) {
 
         getElement().setAttribute("theme", Material.DARK);
-        setId("store-layout");
+        setId("order-layout");
 
-        Image storeImage = new Image(data.getImages().stream().filter(img -> img.getType() == StoreImageType.BACKGROUND_IMAGE).findFirst().get().getUrl(), data.getStoreName());
-        storeImage.addClassName("store-image-field");
-
-        VerticalLayout imageLayout = new VerticalLayout();
-        imageLayout.setAlignItems(Alignment.CENTER);
-        imageLayout.add(storeImage);
-        imageLayout.addClassName("store-image-layout");
+        GridLayout gridLayout = new GridLayout();
 
         VerticalLayout infoLayout = new VerticalLayout();
         infoLayout.setAlignItems(Alignment.CENTER);
-        infoLayout.addClassName("store-info-layout");
-        final H2 nameHeader = new H2(data.getStoreName());
-        nameHeader.addClassName("store-name-header");
-        final Paragraph descriptionParagraph = new Paragraph(data.getAbout());
-        descriptionParagraph.addClassName("store-description-paragraph");
+        infoLayout.addClassName("order-info-layout");
+        final H2 nameHeader = new H2(data.getCustomer().getFirstName() + " " + data.getCustomer().getFirstName());
+        nameHeader.addClassName("order-name-header");
+        final Paragraph descriptionParagraph = new Paragraph(data.getNote());
+        descriptionParagraph.addClassName("order-description-paragraph");
         infoLayout.add(nameHeader, descriptionParagraph);
+
+
+
 
         VerticalLayout actionLayout = new VerticalLayout();
         actionLayout.setAlignItems(Alignment.CENTER);
-        actionLayout.addClassName("store-action-layout");
-        Button viewMenuButton = new Button("View Menu", onClick -> {
-            new Notification("View Menu is clicked .. ");
+        actionLayout.addClassName("order-action-layout");
+        Button viewMenuButton = new Button("Quick Re-Order", onClick -> {
+            new Notification("Order with" + data.getPriceTotal() + " is reordered .. ");
         });
-        viewMenuButton.addClassName("store-view-menu-button");
+        viewMenuButton.addClassName("order-view-menu-button");
         actionLayout.add(viewMenuButton);
 
-        add(imageLayout, infoLayout, actionLayout);
+        add(infoLayout, actionLayout);
     }
 }
