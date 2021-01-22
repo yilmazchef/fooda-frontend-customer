0055f179e8287b46bbe0c81098216decab9c356b
ozdemir-ahmet <ozdemir76@yahoo.com>
2021-01-22 01:56:41 +0100

    Basket related updates.

--- src/main/java/be/fooda/frontend/views/basket/BasketAddressView.java
+++ src/main/java/be/fooda/frontend/views/basket/BasketAddressView.java
@@ -1,10 +1,17 @@
 package be.fooda.frontend.views.basket;
 
+import be.fooda.frontend.components.BasketAddressLayout;
+import be.fooda.frontend.models.basket.BasketAddress;
 import be.fooda.frontend.service.BasketService;
 import be.fooda.frontend.views.main.MainView;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 
 @Route(value = "basket_address", layout = MainView.class)
 @PageTitle("View Basket Address")
@@ -13,13 +20,35 @@ public class BasketAddressView extends VerticalLayout {
     private BasketService basketService;
 
     private Long externalUserId = 1L;
-    private String session = "1";
-    private Long externalStoreId = 1L;
+    private boolean hasAddressesInBasket = false;
+
 
     public BasketAddressView(BasketService basketService) {
         setId("basket-address-view");
 
+        final ResponseEntity<BasketAddress[]> apiResponse = basketService.getAddressesByExternalUserId(externalUserId);
+        BasketAddress[] basketAddresses = apiResponse.getBody();
 
+        if (apiResponse.getStatusCode().equals(HttpStatus.FOUND) && apiResponse.hasBody() && basketAddresses.length > 0) {
+            hasAddressesInBasket = true;
+
+            VerticalLayout addressGroupLayout = new VerticalLayout();
+            addressGroupLayout.addClassName("basket-address-group-layout");
+            for (BasketAddress address : basketAddresses) {
+                    addressGroupLayout.add(new BasketAddressLayout(address));
+            }
+            add(addressGroupLayout);
+        }
+        Button addNewAddressButton = new Button("+ Add New Address");
+
+        HorizontalLayout buttonsLayout = new HorizontalLayout();
+        buttonsLayout.setClassName("basket-address-buttonsLayout");
+        Button backButton = new Button("< Back To Basket");
+        backButton.setClassName("basket-address-back-button");
+        Button nextButton = new Button("Next >");
+        backButton.setClassName("basket-address-next-button");
+        buttonsLayout.add(backButton, nextButton);
+
+        add(addNewAddressButton, buttonsLayout);
     }
-
 }
